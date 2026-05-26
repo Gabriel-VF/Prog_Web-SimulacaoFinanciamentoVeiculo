bcb1f30c02a234a8f79f6189608b5cec7db7da1b
Gabriel Veríssimo <vergabriel07@gmail.com>
2026-05-26 19:50:50 +0000

    Update SimulationServlet.java
    Removidos comentários e detalhes não importantes

--- financiamento_veiculo/src/main/java/financiamento_veiculo/servlet/SimulationServlet.java
+++ financiamento_veiculo/src/main/java/financiamento_veiculo/servlet/SimulationServlet.java
@@ -9,7 +9,6 @@ import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.math.*;
-@SuppressWarnings("unused")
 
 @WebServlet("/dados")
 public class SimulationServlet extends HttpServlet {
@@ -17,11 +16,10 @@ public class SimulationServlet extends HttpServlet {
 	protected void doPost(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
 		double valorVeiculo = Double.parseDouble(req.getParameter("valorVeiculo"));
 		double entrada = Double.parseDouble(req.getParameter("entrada"));
-		double juros = Double.parseDouble(req.getParameter("juros")) / 100; // Porcentagem mensal
+		double juros = Double.parseDouble(req.getParameter("juros")) / 100;
 		int prazo =  Integer.parseInt(req.getParameter("prazo"));
 
 		double valorFinanciado = valorVeiculo - entrada;
-		// P = (PV × i) ÷ [1 – (1 + i)^-n]
 		double p = (valorFinanciado * juros)/(1 - Math.pow(( 1 + juros ), prazo*-1));
 
 		
@@ -36,47 +34,3 @@ public class SimulationServlet extends HttpServlet {
 		req.getRequestDispatcher("results.jsp").forward(req, resp);
 	}
 }
-
-
-/*
- * 	<p>Valor total pago: ${valorTotalPago}</p>
-	<p>Valor total de parcelas: ${valorTotalParcelas}</p>
-	<p>Valor total de juros: ${valorTotalJuros}</p>
- */
-
-/* 
- * (valorcarro *)
- */
-
- /*
-  * P = (PV × i) ÷ [1 – (1 + i)^-n]
-  */
-
-
-
-
-/*
- * 
-    P = Valor da parcela
-    PV = Valor financiado
-    I = Taxa de juros ao período
-    N = Número total de parcelas
-
-A fórmula é:
-
-    P = PV x [ i / (1 - (1 + i)-n ) ]
- */
-
-
-
-
- /*
-  * P = (PV * i) / [1 – (1 + i)^-n]
-
-Onde:
-
-    PV: Valor Presente (valor do empréstimo)
-    i: Taxa de juros por período
-    n: Número total de parcelas
-
-  */
